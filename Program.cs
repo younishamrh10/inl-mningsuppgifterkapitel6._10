9f86a26d47e788d1ab96b0855dadb2c9d3a5a260
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-03-01 08:42:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace inlämningsuppgifterkapitel6._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en sträng");
+            string text = Console.ReadLine();
+            Console.WriteLine("Hur hur många texter de är: " + AntalTalIText(text));
+        }
+
+        static int AntalTalIText(string text)
+        {
+            string[] delar = text.Split(' ');
+            int antal = 0;
+
+            for (int i = 0; i < delar.Length; i++)
+            {
+                double tal;
+                if (double.TryParse(delar[i], out tal))
+                {
+                    antal++;
+                }
+            }
+            return antal;
+        }
+    }
+}
